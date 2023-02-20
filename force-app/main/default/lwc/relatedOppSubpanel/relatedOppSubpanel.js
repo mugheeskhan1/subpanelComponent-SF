32a42b82c983ad71fd61f42649f5362b5f1d42d3
mugheeskhan1 <mugheeskhan36@gmail.com>
2023-02-20 18:32:06 +0500

    fetched data from db

--- force-app/main/default/lwc/relatedOppSubpanel/relatedOppSubpanel.js
+++ force-app/main/default/lwc/relatedOppSubpanel/relatedOppSubpanel.js
@@ -1,3 +1,38 @@
-import { LightningElement } from 'lwc';
+import { getRelatedListRecords } from 'lightning/uiRelatedListApi';
+import { api, LightningElement, wire } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class RelatedOppSubpanel extends LightningElement {}
+export default class RelatedOppSubpanel extends NavigationMixin(LightningElement) {
+    error;
+    opportunities;
+    totalOpp = 0;
+    @api recordId;
+    @wire(getRelatedListRecords, {
+        parentRecordId: '$recordId',
+        relatedListId: 'Opportunities',
+        fields: ['Opportunity.Id', 'Opportunity.Name', 'Opportunity.StageName', 'Opportunity.Amount', 'Opportunity.CloseDate'],
+        where: "{ and: [{ StageName: {like: 'Closed Won'}}] }"
+    }) listInfo({ error, data }) {
+        if (data) {
+            this.opportunities = data.records;
+            this.error = undefined;
+            this.totalOpp = Object.keys(this.opportunities).length;
+        } else if (error) {
+            this.error = error;
+            this.opportunities = undefined;
+        }
+    }
+
+    viewRecord(event) {
+        event.preventDefault();
+        console.log('id => ' + event.target.dataset.id);
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                "recordId": event.target.dataset.id,
+                "objectApiName": "Opportunity",
+                "actionName": "view"
+            },
+        });
+    }
+}
